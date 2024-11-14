5aeeaf130f9db4a00c11b37e1ec52f065c08fa3e
Pheonix2507 <chintub2507@gmail.com>
2024-11-14 16:06:13 +0530

    done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('fetchMessage').addEventListener('click', async () => {
   try {
-    const response = await fetch('https://back-088l.onrender.com');  // Replace with your Render backend URL
+    const response = await fetch('https://back-088l.onrender.com/api/message');  // Replace with your Render backend URL
     const data = await response.json();
     document.getElementById('message').textContent = data.message;
   } catch (error) {
